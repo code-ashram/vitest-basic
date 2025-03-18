f44b746b2bb88da32e9e6b8d1410e03c53937b67
Andrei Yeutukhou <gavnorock84@hotmail.com>
2025-03-18 21:50:20 +0300

    remove odd code from main.ts

--- src/loop/task-1/task-1.test.ts
+++ src/loop/task-1/task-1.test.ts
@@ -1,11 +1,11 @@
 import { describe, expect, it, vi } from 'vitest'
-import { getSumAndProduct } from '../../main'
+import getSumAndProduct from './task-1.ts'
 
 describe('getAmount', () => {
   it('Sum and product of the digits of 1488', () => {
     const consoleSpy = vi.spyOn(console, 'log')
 
-    getSumAndProduct(1288)
+    getSumAndProduct(1488)
 
     expect(consoleSpy).toHaveBeenCalledWith('Sum: 21, product: 256')
     expect(consoleSpy).toHaveBeenCalledTimes(1)
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,1 @@
 import './style.css'
-
-export const getSumAndProduct = (value: number): void => {
-  const numberToString: string = value.toString() // use this variable in your code
-  let sum: number = 0
-  let prod: number = 1
-  // add code below
-
-  // add code above
-  console.log(`Sum: ${sum}, product: ${prod}`)
-}
