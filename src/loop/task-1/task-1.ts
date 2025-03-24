4d5b5e5e5a941e8e6fcfd2a55ce41fcfa9947704
Andrei Yeutukhou <gavnorock84@hotmail.com>
2025-03-24 23:45:12 +0300

    task 1 ready

--- src/loop/task-1/task-1.ts
+++ src/loop/task-1/task-1.ts
@@ -1,20 +1,14 @@
 export const logMultiplicationTable = () => {
   for (let i = 1; i < 10; i++) {
-    // let line = ''
-    //
-    // for (let j = 2; j < 10; j++) {
-    //   line += `${j} x ${i} = ${(j * i + '').padStart(2, ' ')}\t`
-    //   if (j < 9) line += '\t'
-    // }
     let line = []
 
     for (let j = 2; j < 10; j++) {
-      line.push(`${j} x ${i} = ${(j * i + '').toString()}`.padStart(2, ' '))
+      line.push(`${j} x ${i} = ${(j * i + '')}`.padStart(2, ' '))
     }
 
-    line.join('\t')
+    const result = line.join('\t')
 
-    console.log(line)
+    console.log(result)
   }
 }
 
